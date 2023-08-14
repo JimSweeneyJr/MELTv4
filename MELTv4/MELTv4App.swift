a9db0f91095f28875416410c7506fb9f27c1cc86
JimSweeneyJr <jimsweeney566@gmail.com>
2023-08-13 20:30:41 -0400

    first commit

---
+++ MELTv4/MELTv4App.swift
@@ -0,0 +1,17 @@
+//
+//  MELTv4App.swift
+//  MELTv4
+//
+//  Created by James Sweeney on 8/13/23.
+//
+
+import SwiftUI
+
+@main
+struct MELTv4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
