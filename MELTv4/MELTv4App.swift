b6f557104828247d36ddc9e35d30baf16db1efbb
JimSweeneyJr <jimsweeney566@gmail.com>
2023-09-12 19:11:44 -0400

    Added Firebase

--- MELTv4/MELTv4App.swift
+++ MELTv4/MELTv4App.swift
@@ -6,9 +6,24 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
+
 struct MELTv4App: App {
+    
+    // register app delegate for Firebase setup
+      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             SplashScreenView()
