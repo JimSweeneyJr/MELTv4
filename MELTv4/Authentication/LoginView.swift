16ad452950a5a8ce209bd96fe85f36bf8518aceb
JimSweeneyJr <jimsweeney566@gmail.com>
2023-09-18 20:13:03 -0400

    trying to add log in page

---
+++ MELTv4/Authentication/LoginView.swift
@@ -0,0 +1,125 @@
+//
+//  LoginView.swift
+//  MELTv4
+//
+//  Created by James Sweeney on 9/18/23.
+//
+
+import SwiftUI
+import Firebase
+
+class FirebaseManager: NSObject {
+    
+    let auth: Auth
+    
+    static let shared = FirebaseManager()
+    
+    override init() {
+        FirebaseApp.configure()
+        
+        self.auth = Auth.auth()
+        
+        super.init()
+    }
+    
+}
+
+struct LoginView: View {
+    
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                
+                VStack(spacing: 12){
+                    Picker(selection: $isLoginMode, label:
+                            Text("Picker here")) {
+                            Text("Login")
+                            .tag(true)
+                            Text("Create Account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                        .padding()
+                    
+                    if !isLoginMode {
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 64))
+                                .padding()
+                        }
+                    }
+                   
+                    Group {
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                        SecureField("Password", text: $password)
+                            
+                    }
+                    
+                    .padding(12)
+                    .background(Color.white)
+            
+                    Button {
+                        handleAction()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text(isLoginMode ? "Log In" : "Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .font(.system(size: 14, weight: .semibold))
+                            Spacer()
+                        }
+                        .background(Color.blue)
+                    }
+                }
+                .navigationTitle(isLoginMode ? "Log In" : "Create Account")
+                
+                }
+            .padding()
+            .background(Color(.init(white: 0, alpha: 0.05)))
+
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+    private func handleAction() {
+        if isLoginMode {
+            print("Should log into Firebase with existing credentials")
+            loginUser()
+        } else {
+            createNewAccount()
+            
+        }
+    }
+    
+    private func loginUser() {
+        FirebaseManager.shared.auth.signIn(withEmail: email, password: password)
+    }
+    
+    @State var errorMessage = ""
+    
+    private func createNewAccount() {
+        Auth.auth().createUser(withEmail: email, password: password) {
+            result, error in
+            if let err = error {
+                print("Failed to create user", err)
+                self.errorMessage = "Failed to create user"
+                return
+            }
+            print("Successfully created user: \(result?.user.uid ?? "")")
+        }
+    }
+}
+    
+struct LoginView_Preview: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
